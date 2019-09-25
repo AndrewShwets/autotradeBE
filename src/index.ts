661dffcd8150972856fddf960cc1939110b5e6d4
andrewshwets <andrewshwets@gmail.com>
2019-09-24 20:20:37 -0400

    Starting node js express

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import dotenv from "dotenv";
+import express from "express";
+
+// initialize configuration
+dotenv.config();
+
+const app = express();
+
+// define a route handler for the default home page
+app.get("/", (req, res) => {
+  res.json({ express: "Hello world!" });
+});
+
+// start the Express server
+app.listen(process.env.SERVER_PORT, () => {
+  // console.log( `server started at http://localhost:${ port }` );
+});
